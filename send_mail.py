e73d8f180be7ac4f4bcd012dba65e39f7de814b9
George Kimagut <geojimagut@gmail.com>
2024-09-04 18:48:46 +0300

    /mail via mailtrap.io/

--- send_mail.py
+++ send_mail.py
@@ -1,8 +1,21 @@
 import smtplib
-from email.mine.text import MIMEText
+from email.mime.text import MIMEText
 
 def send_mail(customer, dealer, rating, comments):
     port = 2525
     smtp_server = 'smtp.mailtrap.io'
     login = ''
     password = ''
+    message= f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li> <h3>New Feedback Submission</h3><ul><li>Dealer: {dealer}</li> <h3>New Feedback Submission</h3><ul><li>Rating: {rating}</li> <h3>New Feedback Submission</h3><ul><li>Comments: {comments}</li>"  #easy way to have variables is by using f
+
+    sender_email ='email@example.com'
+    receiver_email = 'email2@example.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Lexus Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    #send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
